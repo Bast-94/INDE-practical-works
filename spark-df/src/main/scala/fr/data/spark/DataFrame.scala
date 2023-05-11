bcfa051f23e9dbdd67871b8500edd561b03ca463
Bast-94 <bh.69@live.fr>
2023-05-11 18:58:03 +0200

    FIRST(spark-df).

---
+++ spark-df/src/main/scala/fr/data/spark/DataFrame.scala
@@ -0,0 +1,13 @@
+package fr.data.spark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object DataFrame {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+}
